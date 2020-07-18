dc39c6644e2cd7c8992b5d20e967dfc2ce9fc64a
changguang.peng <1024potato@gmail.com>
2020-07-18 17:14:06 +0800

    no message

---
+++ demo-spring-boot-starter/src/main/java/cn/kj120/study/StarterConfig.java
@@ -0,0 +1,22 @@
+package cn.kj120.study;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// application.properties 配置文件中存在 demoStarter 配置才自动注入
+@ConditionalOnProperty(value = "potato.name", havingValue = "potato")
+@EnableConfigurationProperties(UserConfig.class)
+public class StarterConfig {
+
+    @Autowired
+    private UserConfig userConfig;
+
+    @Bean
+    public DemoService demoService() {
+        return new DemoService(userConfig);
+    }
+}
---
+++ spring-mv-mini/README.md
@@ -0,0 +1,7 @@
+#### 乞丐版MVC实现
+
+![](http://qiniu.kj120.cn/65867c3e3fb105ca74fca4264691c81.png)
+
+![image-20200709100617514](http://qiniu.kj120.cn/image-20200709100617514.png)
+
+![image-20200709100656905](http://qiniu.kj120.cn/image-20200709100656905.png)
